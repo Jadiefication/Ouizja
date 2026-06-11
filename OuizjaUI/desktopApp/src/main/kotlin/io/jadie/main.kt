02f0e4767e598abb6372de93f2df091c8e6a9773
jadie <jadeczjade008@gmail.com>
2026-06-11 20:06:35 +0200

    chore: base UI setup

---
+++ OuizjaUI/desktopApp/src/main/kotlin/io/jadie/main.kt
@@ -0,0 +1,13 @@
+package io.jadie
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "OuizjaUI",
+    ) {
+        App()
+    }
+}
---
+++ OuizjaUI/shared/src/commonMain/kotlin/io/jadie/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package io.jadie
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
