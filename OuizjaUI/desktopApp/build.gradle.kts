49ac8c8a17a4f611e21bbdc2a938cb9057576d6a
jadie <jadeczjade008@gmail.com>
2026-07-23 23:01:51 +0200

    chore: logos

--- OuizjaUI/desktopApp/build.gradle.kts
+++ OuizjaUI/desktopApp/build.gradle.kts
@@ -19,12 +19,34 @@ dependencies {
 
 compose.desktop {
     application {
+        buildTypes.release.proguard {
+            isEnabled.set(false)
+        }
         mainClass = "io.jadie.MainKt"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "io.jadie"
+            packageName = "Ouizja"
             packageVersion = "1.0.0"
+            description = "High-Performance Thermal Simulation Engine"
+            vendor = "Jadie"
+            copyright = "© 2026 Jadie. All rights reserved."
+            includeAllModules = true
+
+            macOS {
+                iconFile.set(project.file("src/resources/app_icon.icns"))
+                bundleID = "io.jadie.ouizja"
+            }
+            windows {
+                iconFile.set(project.file("src/resources/app_icon.ico"))
+                shortcut = true
+                menuGroup = "Ouizja"
+            }
+            linux {
+                iconFile.set(project.file("src/resources/icon.png"))
+                shortcut = true
+            }
         }
     }
 }
